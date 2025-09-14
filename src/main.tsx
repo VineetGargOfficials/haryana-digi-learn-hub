d534565fe7ad4833d0dbe4ffc1f23f533d581a48
Vineet Garg <contact.vineetgarg@gmail.com>
2025-09-14 18:38:24 +0530

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./Form.tsx";
+import App from "./Login.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
